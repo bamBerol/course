ce1482d7ea9b656304af789ba1209c1844f82376
Dawid T <geekoonn@gmail.com>
2024-01-22 18:57:48 +0100

    bla bla

--- dolor.js
+++ dolor.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 console.log("dolor sit amed");
 console.log("dolor * 2");
-=======
 const result = 10 + 20;
 console.log("calculating");
 console.log(`result is ${result}`);
->>>>>>> main
+s;
